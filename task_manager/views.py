1edf7a6b4b5f97fc009dd34a2dc40feccc501772
Olga <nemo0mb@gmail.com>
2026-03-11 15:28:37 +0300

    Add translations for login/logout messages

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
+from django.http import HttpResponse
 
 
 class IndexView(TemplateView):
@@ -14,8 +15,9 @@ class LoginUserView(LoginView):
     next_page = reverse_lazy('index')
     
     def form_valid(self, form):
+        response = super().form_valid(form)
         messages.success(self.request, _('You are logged in'))
-        return super().form_valid(form)
+        return response
 
 
 class LogoutUserView(LogoutView):
@@ -24,8 +26,6 @@ class LogoutUserView(LogoutView):
     def dispatch(self, request, *args, **kwargs):
         messages.info(request, _('You are logged out'))
         return super().dispatch(request, *args, **kwargs)
-
-from django.http import HttpResponse
 
 
 def trigger_error(request):
