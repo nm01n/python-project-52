48bc6c53aaad29580c2f3b71496eae4802aa4a22
Olga <nemo0mb@gmail.com>
2026-03-15 18:54:36 +0300

    Fix linter errors

--- task_manager/users_urls.py
+++ task_manager/users_urls.py
@@ -3,8 +3,24 @@ from django.urls import path
 from task_manager import users_views
 
 urlpatterns = [
-    path('', users_views.UserListView.as_view(), name='users_list'),
-    path('create/', users_views.UserCreateView.as_view(), name='user_create'),
-    path('<int:pk>/update/', users_views.UserUpdateView.as_view(), name='user_update'),
-    path('<int:pk>/delete/', users_views.UserDeleteView.as_view(), name='user_delete'),
+    path(
+	'',
+        users_views.UserListView.as_view(),
+        name='users_list'
+        ),
+    path(
+        'create/',
+        users_views.UserCreateView.as_view(),
+        name='user_create'
+        ),
+    path(
+        '<int:pk>/update/',
+        users_views.UserUpdateView.as_view(),
+        name='user_update'
+        ),
+    path(
+        '<int:pk>/delete/',
+        users_views.UserDeleteView.as_view(),
+        name='user_delete'
+        ),
 ]
